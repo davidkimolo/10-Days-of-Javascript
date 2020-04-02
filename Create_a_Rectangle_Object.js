5a6ce8544df28bfb8f15f1ea8e88921c92b65f21
David Kimolo <mwikyakimolo@gmail.com>
2020-04-02 11:43:55 +0300

    Create a Rectangle Object

---
+++ Create_a_Rectangle_Object.js
@@ -0,0 +1,16 @@
+
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    let rectangle = {
+        length: a,
+        width: b,
+        perimeter: 2 *(a+b),
+        area: a*b
+    }
+
+    return rectangle;
+}
+
